5194ff54691916c31faca11dea0d28a5be929474
cycas <172155721@qq.com>
2020-02-16 16:22:50 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: "铁树的博客",
+  description: "这是铁树的新博客，使用VuePress搭建。前端技术博客。",
+  base: "/cycas_blog/",
+  repo: "vuejs/vuepress",
+  repoLabel: "查看源码",
+  locales: {
+    "/": {
+      lang: "zh-CN"
+    }
+  },
+  themeConfig: {
+    lastUpdated: "上次更新",
+    sidebar: "auto",
+    nav: [
+      { text: "主页", link: "/" },
+      { text: "coding心得", link: "/coding/" },
+      { text: "文摘", link: "/digest/" }
+    ]
+  }
+};
